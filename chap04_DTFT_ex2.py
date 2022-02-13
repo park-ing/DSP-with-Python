0587e836d057f0c7043ae133850ae5df36a164a3
park-ing <yh.park8742@gmail.com>
2022-02-13 13:43:28 +0900

    v2

--- chap04_DTFT_ex2.py
+++ chap04_DTFT_ex2.py
@@ -2,9 +2,10 @@ import numpy as np
 import matplotlib.pylab as plt
 
 N = 500
-n = np.arange(30)
+n = np.arange(30) # array([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,....])
 xn = np.power(0.5, n)
-Omega = np.arange(-2*N,2*N)*2*np.pi/N
+Omega = np.arange(-2*N,2*N)*2*np.pi/N   # 오메가 값들을 많이 얻기 위하여
+print(Omega)
 X = np.exp(1j*Omega)/(np.exp(1j*Omega)-0.5)
 magX = np.abs(X)
 
@@ -12,5 +13,6 @@ plt.subplot(2,1,1); plt.stem(n,xn,"b"); plt.grid()
 plt.ylabel("x(n)");plt.title("x(n) & X(Omega), DTFT of x(n)")
 plt.subplot(2,1,2); plt.plot(Omega, magX, "b")
 plt.xlabel("Omega, frequency in radians"); plt.ylabel("|X(Omega)|");
-plt.xlim(-4*np.pi,4*np.pi);plt.grid()
+plt.xlim(-4*np.pi,4*np.pi);
+plt.grid()
 plt.show()
