953e9b42bc10c6828c51ff8e5217136148a27318
park-ing <yh.park8742@gmail.com>
2022-02-13 16:52:03 +0900

    v1

---
+++ chap04_DFT_IDFT.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def DFT(xn, N): # 이산푸리에변환
+    WN = np.exp(-1j*(2*np.pi/N))  # 회전인자 계산
+    Xk = np.zeros(N, dtype="complex64")   # DFT 계수 어레이 설정
+    for i in range(N):
+        sum = 0
+        for j in range(N):
+            sum = sum + xn[j]*np.power(WN,i*j)  # DFT 계수 계산
+        Xk[i] = sum
+    return Xk   # DFT 계수 리턴
+
+def IDFT(Xk, N): # 역 이산푸리에변환
+    WN = np.exp(-1j*(2*np.pi/N))  # 회전인자 계산
+    xn = np.zeros(N, dtype="complex64")   # 복원 이산신호 어레이 설정
+    for i in range(N):
+        sum = 0
+        for j in range(N):
+            sum = sum + Xk[j]*np.power(WN,-i*j)  # 역 DFT 계산
+        xn[i] = sum/N
+    return xn   # IDFT 계수 리턴    
