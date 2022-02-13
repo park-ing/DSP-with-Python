cd6d6c148fe0d145fed2f6c16ac297b352a713aa
park-ing <yh.park8742@gmail.com>
2022-02-13 16:59:59 +0900

    v2

--- chap04_DFT_IDFT.py
+++ chap04_DFT_IDFT.py
@@ -19,3 +19,13 @@ def IDFT(Xk, N): # 역 이산푸리에변환
             sum = sum + Xk[j]*np.power(WN,-i*j)  # 역 DFT 계산
         xn[i] = sum/N
     return xn   # IDFT 계수 리턴    
+
+xn = [1,1,1,1];print("xn=",xn)
+N = 4
+Xk = DFT(xn,N);print("Xk=",Xk)
+magXk = np.abs(Xk); print("Magnitude of Xk=",magXk)
+phaXk = np.angle(Xk)*180/np.pi
+print("Phase of Xk=", phaXk)
+xn = np.real(IDFT(Xk,N))    # real값 
+xn = IDFT(Xk,N)
+print("Reconstructed xn=",xn)
